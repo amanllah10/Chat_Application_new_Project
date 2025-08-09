8c18f05dadb1be2a72835b2cafbbe5c6e3cf1a00
amanllah10 <amanullah107676@gmail.com>
2025-08-09 01:04:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -104,64 +104,72 @@ const room2 = () => {
 
 
 // createRoom function
-const createRoom = () => {
-    let count = leftSection.childElementCount;
-    let roomIndex = count - 1; // clear naming, no shadowing
+const createRoom = (number) => {
 
-    let chat3Div = document.createElement('div');
-    chat3Div.className = `chat3-rooms room${roomIndex} chat${roomIndex}`;
+    let count = leftSection.childElementCount
 
-    // Agar second last position par insert karna hai:
+    let number = count - 1
+    
+    let chat3Div = document.createElement('div')
+    chat3Div.className = chat3-rooms room${number} chat${number}
+    leftSection.appendChild(chat3Div)
+
+    rightSection.appendChild(Room3)
+    Room3.className = room${number}
+
+    let chat3H2 = document.createElement('h2')
+    chat3H2.className = 'chat3-h2'
+    chat3Div.appendChild(chat3H2)
+
     if (count > 0) {
-        leftSection.insertBefore(chat3Div, leftSection.children[count - 1]);
-    } else {
-        leftSection.appendChild(chat3Div);
+        leftSection.insertBefore(chat3Div, leftSection.children[count - 1])
+        chat3H2.innerHTML = chat Room${count - 1}
     }
 
-    rightSection.appendChild(Room3);
-    Room3.className = `room${roomIndex}`;
+    chatRoomP3.className = chat-room-p${number}
 
-    let chat3H2 = document.createElement('h2');
-    chat3H2.className = 'chat3-h2';
-    chat3Div.appendChild(chat3H2);
-    chat3H2.innerHTML = `chat Room${roomIndex}`;
-
-    chatRoomP3.className = `chat-room-p${roomIndex}`;
 
     chat3Div.addEventListener('click', () => {
-        let allChatTabs = document.querySelectorAll('.chat3-rooms');
+
+        let allChatTabs = document.querySelectorAll('.chat3-rooms')
         allChatTabs.forEach(tab => {
-            tab.classList.remove('bgyellow');
-            tab.classList.add('bgwhite');
-        });
+            tab.classList.remove('bgyellow')
+            tab.classList.add('bgwhite')
+        })
 
-        roomsObject[`rooms${roomIndex}`] = true;
-        room1ChildsDisplay = false;
-        room2ChildsDisplay = false;
+        roomsObject[rooms${number}] = true
 
-        if (userInput.value === '') {
-            Room3.innerHTML = `chat Room ${roomIndex}`;
+        if (roomsObject[rooms${number}]) {
+            room1ChildsDisplay = false
+            room2ChildsDisplay = false
+            roomsObject[rooms${number}] = true
+
+            if (userInput.value === '') {
+                console.log(Room3.innerHTML = chat Room ${count - 1})
+            }
+
+            chat3Div.appendChild(chatRoomP3)
+
+            Room1.className = 'display-none'
+            Room2.className = 'display-none'
+            Room3.className = 'display-block'
+
+            chat1.className = 'chat1 bgwhite'
+            chat2.className = 'chat2 bgwhite'
+            chat3Div.className = chat3-rooms room3 chat${number}
+            chat3Div.classList.remove('bgwhite')
+            chat3Div.classList.add('bgyellow')
+
+
+            chatRoomP1.innerHTML = ''
+            chatRoomP2.innerHTML = ''
+            chatRoomP3.innerHTML = 'online'
+
+        } else {
+
         }
-
-        chat3Div.appendChild(chatRoomP3);
-
-        Room1.className = 'display-none';
-        Room2.className = 'display-none';
-        Room3.className = 'display-block';
-
-        chat1.className = 'chat1 bgwhite';
-        chat2.className = 'chat2 bgwhite';
-        chat3Div.className = `chat3-rooms room3 chat${roomIndex}`;
-        chat3Div.classList.remove('bgwhite');
-        chat3Div.classList.add('bgyellow');
-
-        chatRoomP1.innerHTML = '';
-        chatRoomP2.innerHTML = '';
-        chatRoomP3.innerHTML = 'online';
-    });
-};
-
-
+    })
+}
 
 // sendMessageToRoom function
 const sendMessageToRoom = () => {
