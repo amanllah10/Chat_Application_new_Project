d20d7172bfb0d13008efaa8848a6cb4528b57e97
amanllah10 <amanullah107676@gmail.com>
2025-08-09 01:08:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ const createRoom = (number) => {
 
     let count = leftSection.childElementCount
 
-    let number = count - 1
+    number = count - 1
     
     let chat3Div = document.createElement('div')
     chat3Div.className = chat3-rooms room${number} chat${number}
